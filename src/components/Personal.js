c52276cbc2b183e2d5f494c977e1f87df0348819
josh1510 <joshmoon.1510@gmail.com>
2021-07-26 14:55:10 +1000

    Adding additional personal details fields

--- src/components/Personal.js
+++ src/components/Personal.js
@@ -15,18 +15,57 @@ const Personal = (props) => {
   const classes = useStyles();
   const { personalDetails } = props;
   return (
-      <form className={classes.root} noValidate autoComplete="off"> 
+    <form className={classes.root} noValidate autoComplete="off">
       <TextField
-        variant='outlined'
-        id='firstName'
-        type="text"  
-        name="firstName"
+        variant="outlined"
+        id="usrFirstName"
+        type="text"
+        name="usrFirstName"
         label="First Name"
         value={personalDetails.firstName}
       />
-        </form>
-      
+      <TextField
+        variant="outlined"
+        id="usrLastName"
+        type="text"
+        name="usrLastName"
+        label="Last Name"
+        value={personalDetails.firstName}
+      />
+      <TextField
+        variant="outlined"
+        id="usrEmail"
+        type="text"
+        name="usrEmail"
+        label="Email Address"
+        value={personalDetails.firstName}
+      />
+      <TextField
+        variant="outlined"
+        id="usrPhone"
+        type="text"
+        name="usrPhone"
+        label="Phone Number"
+        value={personalDetails.firstName}
+      />
+      <TextField
+        variant="outlined"
+        id="usrGitHub"
+        type="text"
+        name="usrGitHub"
+        label="GitHub"
+        value={personalDetails.firstName}
+      />
+      <TextField
+        variant="outlined"
+        id="usrLinkedIn"
+        type="text"
+        name="usrLinkedIn"
+        label="LinkedIn"
+        value={personalDetails.firstName}
+      />
+    </form>
   );
-}
+};
 
 export default Personal;
